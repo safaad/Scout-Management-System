0e54b966dc6bf048b487c154de697771d0531cfb
bakernaim <baker_naim_1999@hotmail.com>
2018-12-29 01:37:06 +0200

    first commit

--- src/Controllers/DeanController.java
+++ src/Controllers/DeanController.java
@@ -1,4 +1,18 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
 public class DeanController {
+
+    @FXML
+    AnchorPane pane;
+
+    @FXML
+    public void Add(ActionEvent event) throws Exception {
+        AnchorPane pane1 = FXMLLoader.load(getClass().getResource("../GUI/fram.fxml"));
+        pane.getChildren().add(pane1);
+    }
 }
--- src/Driver/Main.java
+++ src/Driver/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../GUI/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../GUI/Dean.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
