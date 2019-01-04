6756255affbc6177d483504d7bebf609f2fce5ba
bakernaim <baker_naim_1999@hotmail.com>
2019-01-04 22:54:02 +0200

    test

---
+++ src/Controllers/LeaderRemoving/framRemoveController.java
@@ -0,0 +1,25 @@
+package Controllers.LeaderRemoving;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+public class framRemoveController {
+
+    @FXML
+    AnchorPane pane2;
+
+    @FXML
+    public void member(ActionEvent event) throws Exception {
+        AnchorPane pane1 = FXMLLoader.load(getClass().getResource("../../GUI/LeaderRemoving/RemoveMember.fxml"));
+        pane2.getChildren().setAll(pane1);
+    }
+
+    @FXML
+    public void item (ActionEvent event) throws Exception {
+        AnchorPane pane1 = FXMLLoader.load(getClass().getResource("../../GUI/LeaderRemoving/RemoveItem.fxml"));
+        pane2.getChildren().setAll(pane1);
+    }
+
+}
